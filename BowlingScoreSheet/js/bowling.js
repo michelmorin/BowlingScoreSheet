150688d07a4630a4acd01123717a530983b4ee96
michelmorin <michelmorin@10.113.26.58>
2017-12-19 15:21:00 -0500

    start of calculating pin count

--- BowlingScoreSheet/js/bowling.js
+++ BowlingScoreSheet/js/bowling.js
@@ -60,6 +60,45 @@ $(document).ready(function(){
 		$('#'+game+'-'+player+'-'+frame).addClass('active');  
 		selectedFrameID = id;
 		$("#result").html(selectedFrameID + " Game " + game + " Player " + player + " Frame " + frame + " Ball " + ball );
+		
+		//Get pins down from Current and set correctly
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin1 == "0") {
+			$('#pin1').removeClass('pinDown');
+		} 
+		else {
+			$('#pin1').addClass('pinDown');
+		}
+		if (pin2 == "0") {
+			$('#pin2').removeClass('pinDown');
+		} 
+		else {
+			$('#pin2').addClass('pinDown');
+		}
+		if (pin3 == "0") {
+			$('#pin3').removeClass('pinDown');
+		} 
+		else {
+			$('#pin3').addClass('pinDown');
+		}
+		if (pin4 == "0") {
+			$('#pin4').removeClass('pinDown');
+		} 
+		else {
+			$('#pin4').addClass('pinDown');
+		}
+		if (pin5 == "0") {
+			$('#pin5').removeClass('pinDown');
+		} 
+		else {
+			$('#pin5').addClass('pinDown');
+		}
+		
 	}); 
 	$('#bowlingTable td').on('mouseover', function(){
 		$(this).addClass('highlight');
@@ -67,12 +106,9 @@ $(document).ready(function(){
 	$('#bowlingTable td').on('mouseout', function(){
 		$(this).removeClass('highlight');
 	});
-	$('#pindeck').on('click', 'img', function() {
-		$(this).toggleClass('pinDown')
-	});
 	$('#nextBall').on('click', function() {
 		if (selectedFrameID == null) {
-			selectedFrameID = '#1-1-1-1';
+			selectedFrameID = '1-1-1-1';
 			$('#1-1-1-1').click();
 			return;
 		}
@@ -90,7 +126,7 @@ $(document).ready(function(){
 	});
 	$('#prevBall').on('click', function() {
 		if (selectedFrameID == null) {
-			selectedFrameID = '#1-1-1-1';
+			selectedFrameID = '1-1-1-1';
 			$('#1-1-1-1').click();
 			return;
 		}
@@ -106,4 +142,208 @@ $(document).ready(function(){
 		}
 		$('#'+game+'-'+player+'-'+frame+'-'+ball).click();
 	});
+	$('#pin1').on('click', function() {
+		$(this).toggleClass('pinDown')
+		
+		
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0]
+		var player = currentSelectedItem[1]
+		var frame = currentSelectedItem[2]
+		var ball = currentSelectedItem[3]
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin1 == "0") {
+			//Add 2 pins as pin was up
+			pin1 = "1";
+			pinCount = 2;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			pin1 = "0";
+			pinCount = -2;
+		}
+			
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+			
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		$('#'+selectedFrameID).text(newPoints.toString());	
+		
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+	});
+	$('#pin2').on('click', function() {
+		$(this).toggleClass('pinDown')
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0];
+		var player = currentSelectedItem[1];
+		var frame = currentSelectedItem[2];
+		var ball = currentSelectedItem[3];
+		
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin2 == "0") {
+			//Add 2 pins as pin was up
+			pin2 = "1";
+			pinCount = 3;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			pin2 = "0";
+			pinCount = -3;
+		}
+		
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		$('#'+selectedFrameID).text(newPoints.toString());
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+	});
+	$('#pin3').on('click', function() {
+		$(this).toggleClass('pinDown')
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0];
+		var player = currentSelectedItem[1];
+		var frame = currentSelectedItem[2];
+		var ball = currentSelectedItem[3];
+		
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin3 == "0") {
+			//Add 2 pins as pin was up
+			pin3 = "1";
+			pinCount = 5;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			pin3 = "0";
+			pinCount = -5;
+		}
+		
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		$('#'+selectedFrameID).text(newPoints.toString());
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+	});
+	$('#pin4').on('click', function() {
+		$(this).toggleClass('pinDown')
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0];
+		var player = currentSelectedItem[1];
+		var frame = currentSelectedItem[2];
+		var ball = currentSelectedItem[3];
+		
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin4 == "0") {
+			//Add 2 pins as pin was up
+			pin4 = "1";
+			pinCount = 3;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			pin4 = "0";
+			pinCount = -3;
+		}
+		
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		$('#'+selectedFrameID).text(newPoints.toString());
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+	});
+	$('#pin5').on('click', function() {
+		$(this).toggleClass('pinDown')
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0];
+		var player = currentSelectedItem[1];
+		var frame = currentSelectedItem[2];
+		var ball = currentSelectedItem[3];
+		
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		if (pin5 == "0") {
+			//Add 2 pins as pin was up
+			pin5 = "1";
+			pinCount = 2;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			pin5 = "0";
+			pinCount = -2;
+		}
+		
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		$('#'+selectedFrameID).text(newPoints.toString());
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		}
+	});
 });
+
+function onChangeCalculateAllFrameTotals() {
+
+}
