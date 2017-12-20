6db0a13fe760b0dd7955d9778715c677aa1885e8
michelmorin <michelmorin@10.113.26.58>
2017-12-20 10:38:53 -0500

    Changes to scoring to figure strikes and spares, etc

--- BowlingScoreSheet/js/bowling.js
+++ BowlingScoreSheet/js/bowling.js
@@ -46,7 +46,8 @@ $(document).ready(function(){
 		$(this).addClass('active');
 		$('#'+game+'-'+player+'-'+frame+'-1').addClass('active');  
 		selectedFrameID = id+'-1';
-		$("#result").html(selectedFrameID + " Game " + game + " Player " + player + " Frame " + frame + " Ball 1" );
+		
+		setPinDeckOnSelection();	
 	}); 
 	$('#balls td').on('click', function(){
 		var id = $(this).attr('id');
@@ -59,8 +60,158 @@ $(document).ready(function(){
 		$(this).addClass('active');
 		$('#'+game+'-'+player+'-'+frame).addClass('active');  
 		selectedFrameID = id;
-		$("#result").html(selectedFrameID + " Game " + game + " Player " + player + " Frame " + frame + " Ball " + ball );
 		
+		setPinDeckOnSelection();	
+	}); 
+	$('#bowlingTable td').on('mouseover', function(){
+		$(this).addClass('highlight');
+	});
+	$('#bowlingTable td').on('mouseout', function(){
+		$(this).removeClass('highlight');
+	});
+	$('#nextBall').on('click', function() {
+		if (selectedFrameID == null) {
+			selectedFrameID = '1-1-1-1';
+			$('#1-1-1-1').click();
+			return;
+		}
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0]
+		var player = currentSelectedItem[1]
+		var frame = currentSelectedItem[2]
+		var ball = currentSelectedItem[3]
+		var ball = parseInt(ball) + 1;		
+	
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins');
+
+		if (ball == 2 || ball == 3) {
+			if(currentPinsDown == "1-1-1-1-1") {
+				var frame = parseInt(frame) + 1;
+				var ball = 1;
+			}
+		}
+		if (ball == 4) {
+			var frame = parseInt(frame) + 1;
+			var ball = 1;
+		}
+		$('#'+game+'-'+player+'-'+frame+'-'+ball).click();
+	});
+	$('#prevBall').on('click', function() {
+		if (selectedFrameID == null) {
+			selectedFrameID = '1-1-1-1';
+			$('#1-1-1-1').click();
+			return;
+		}
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0]
+		var player = currentSelectedItem[1]
+		var frame = currentSelectedItem[2]
+		var ball = currentSelectedItem[3]
+		ball = parseInt(ball) - 1;
+		
+		if (ball == 0) {
+			frame = parseInt(frame) - 1;
+			ball = 3;
+		}
+		
+		var currentPoints = $('#'+game+'-'+player+'-'+frame+'-'+ball).attr('points');
+		
+		if (ball == 3) {
+			var currentPinsDown = $('#'+game+'-'+player+'-'+frame+'-'+ball).attr('pins');
+
+			if(currentPinsDown == "1-1-1-1-1" && currentPoints == "0") {
+				ball = parseInt(ball) - 1;
+				var currentPoints = $('#'+game+'-'+player+'-'+frame+'-'+ball).attr('points');
+				var currentPinsDown = $('#'+game+'-'+player+'-'+frame+'-'+ball).attr('pins');
+				if(currentPinsDown == "1-1-1-1-1" && currentPoints == "0") {
+					ball = parseInt(ball) - 1;
+				}
+			}	
+		}
+				
+		$('#'+game+'-'+player+'-'+frame+'-'+ball).click();
+	});
+	$('#pin1').on('click', function() {
+		$(this).toggleClass('pinDown')
+		selectedPin(0,2);
+	});
+	$('#pin2').on('click', function() {
+		$(this).toggleClass('pinDown')
+		selectedPin(1,3);
+	});
+	$('#pin3').on('click', function() {
+		$(this).toggleClass('pinDown')
+		selectedPin(2,5);
+	});
+	$('#pin4').on('click', function() {
+		$(this).toggleClass('pinDown')
+		selectedPin(3,3);
+	});
+	$('#pin5').on('click', function() {
+		$(this).toggleClass('pinDown')		
+		selectedPin(4,2);	
+	});
+	
+	function selectedPin() {
+		var pinNumber = arguments[0];
+		var pinValue = arguments[1];
+		
+		
+		var currentSelectedItem = selectedFrameID.split("-");
+		var game = currentSelectedItem[0];
+		var player = currentSelectedItem[1];
+		var frame = currentSelectedItem[2];
+		var ball = currentSelectedItem[3];
+		
+		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
+
+		if (currentPinsDown[pinNumber] == "0") {
+			//Add 2 pins as pin was up
+			currentPinsDown[pinNumber] = "1";
+			pinCount = pinValue;
+		} 
+		else {
+			//Subtract 2 pins as pin was down
+			currentPinsDown[pinNumber] = "0";
+			pinCount = -pinValue;
+		}
+		
+		var pin1 = currentPinsDown[0];
+		var pin2 = currentPinsDown[1];
+		var pin3 = currentPinsDown[2];
+		var pin4 = currentPinsDown[3];
+		var pin5 = currentPinsDown[4];
+		
+		var currentPoints = $('#'+selectedFrameID).attr('points');
+		var newPoints = parseInt(currentPoints) + pinCount;
+		$('#'+selectedFrameID).attr('points', newPoints);
+		
+		if (pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5 == "1-1-1-1-1" && ball == "1") {
+			$('#'+selectedFrameID).text("X");
+		}
+		else if (pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5 == "1-1-1-1-1" && ball == "2") {
+			$('#'+selectedFrameID).text("/");
+		}
+		else {
+			$('#'+selectedFrameID).text(newPoints.toString());
+		}
+		
+		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+		ball = parseInt(ball) + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('points', "0");
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).text("");
+		}
+		ball = ball + 1;
+		if (ball < 4 ) {
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('points', "0");
+			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).text("");
+		}
+	}
+	
+	function setPinDeckOnSelection() {
 		//Get pins down from Current and set correctly
 		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
 		var pin1 = currentPinsDown[0];
@@ -98,252 +249,12 @@ $(document).ready(function(){
 		else {
 			$('#pin5').addClass('pinDown');
 		}
-		
-	}); 
-	$('#bowlingTable td').on('mouseover', function(){
-		$(this).addClass('highlight');
-	});
-	$('#bowlingTable td').on('mouseout', function(){
-		$(this).removeClass('highlight');
-	});
-	$('#nextBall').on('click', function() {
-		if (selectedFrameID == null) {
-			selectedFrameID = '1-1-1-1';
-			$('#1-1-1-1').click();
-			return;
-		}
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0]
-		var player = currentSelectedItem[1]
-		var frame = currentSelectedItem[2]
-		var ball = currentSelectedItem[3]
-		var ball = parseInt(ball) + 1;		
-		if (ball == 4) {
-			var frame = parseInt(frame) + 1;
-			var ball = 1;
-		}
-		$('#'+game+'-'+player+'-'+frame+'-'+ball).click();
-	});
-	$('#prevBall').on('click', function() {
-		if (selectedFrameID == null) {
-			selectedFrameID = '1-1-1-1';
-			$('#1-1-1-1').click();
-			return;
-		}
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0]
-		var player = currentSelectedItem[1]
-		var frame = currentSelectedItem[2]
-		var ball = currentSelectedItem[3]
-		var ball = parseInt(ball) - 1;
-		if (ball == 0) {
-			var frame = parseInt(frame) - 1;
-			var ball = 3;
-		}
-		$('#'+game+'-'+player+'-'+frame+'-'+ball).click();
-	});
-	$('#pin1').on('click', function() {
-		$(this).toggleClass('pinDown')
-		
-		
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0]
-		var player = currentSelectedItem[1]
-		var frame = currentSelectedItem[2]
-		var ball = currentSelectedItem[3]
-		var currentPoints = $('#'+selectedFrameID).attr('points');
+	}
+	
+	function onChangeCalculateAllFrameTotals() {
 
-		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
-		var pin1 = currentPinsDown[0];
-		var pin2 = currentPinsDown[1];
-		var pin3 = currentPinsDown[2];
-		var pin4 = currentPinsDown[3];
-		var pin5 = currentPinsDown[4];
-		if (pin1 == "0") {
-			//Add 2 pins as pin was up
-			pin1 = "1";
-			pinCount = 2;
-		} 
-		else {
-			//Subtract 2 pins as pin was down
-			pin1 = "0";
-			pinCount = -2;
-		}
-			
-		var currentPoints = $('#'+selectedFrameID).attr('points');
-			
-		var newPoints = parseInt(currentPoints) + pinCount;
-		$('#'+selectedFrameID).attr('points', newPoints);
-		$('#'+selectedFrameID).text(newPoints.toString());	
-		
-		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		ball = parseInt(ball) + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-		ball = ball + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-	});
-	$('#pin2').on('click', function() {
-		$(this).toggleClass('pinDown')
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0];
-		var player = currentSelectedItem[1];
-		var frame = currentSelectedItem[2];
-		var ball = currentSelectedItem[3];
-		
-		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
-		var pin1 = currentPinsDown[0];
-		var pin2 = currentPinsDown[1];
-		var pin3 = currentPinsDown[2];
-		var pin4 = currentPinsDown[3];
-		var pin5 = currentPinsDown[4];
-		if (pin2 == "0") {
-			//Add 2 pins as pin was up
-			pin2 = "1";
-			pinCount = 3;
-		} 
-		else {
-			//Subtract 2 pins as pin was down
-			pin2 = "0";
-			pinCount = -3;
-		}
-		
-		var currentPoints = $('#'+selectedFrameID).attr('points');
-		var newPoints = parseInt(currentPoints) + pinCount;
-		$('#'+selectedFrameID).attr('points', newPoints);
-		$('#'+selectedFrameID).text(newPoints.toString());
-		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		ball = parseInt(ball) + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-		ball = ball + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-	});
-	$('#pin3').on('click', function() {
-		$(this).toggleClass('pinDown')
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0];
-		var player = currentSelectedItem[1];
-		var frame = currentSelectedItem[2];
-		var ball = currentSelectedItem[3];
-		
-		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
-		var pin1 = currentPinsDown[0];
-		var pin2 = currentPinsDown[1];
-		var pin3 = currentPinsDown[2];
-		var pin4 = currentPinsDown[3];
-		var pin5 = currentPinsDown[4];
-		if (pin3 == "0") {
-			//Add 2 pins as pin was up
-			pin3 = "1";
-			pinCount = 5;
-		} 
-		else {
-			//Subtract 2 pins as pin was down
-			pin3 = "0";
-			pinCount = -5;
-		}
-		
-		var currentPoints = $('#'+selectedFrameID).attr('points');
-		var newPoints = parseInt(currentPoints) + pinCount;
-		$('#'+selectedFrameID).attr('points', newPoints);
-		$('#'+selectedFrameID).text(newPoints.toString());
-		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		ball = parseInt(ball) + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-		ball = ball + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-	});
-	$('#pin4').on('click', function() {
-		$(this).toggleClass('pinDown')
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0];
-		var player = currentSelectedItem[1];
-		var frame = currentSelectedItem[2];
-		var ball = currentSelectedItem[3];
-		
-		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
-		var pin1 = currentPinsDown[0];
-		var pin2 = currentPinsDown[1];
-		var pin3 = currentPinsDown[2];
-		var pin4 = currentPinsDown[3];
-		var pin5 = currentPinsDown[4];
-		if (pin4 == "0") {
-			//Add 2 pins as pin was up
-			pin4 = "1";
-			pinCount = 3;
-		} 
-		else {
-			//Subtract 2 pins as pin was down
-			pin4 = "0";
-			pinCount = -3;
-		}
-		
-		var currentPoints = $('#'+selectedFrameID).attr('points');
-		var newPoints = parseInt(currentPoints) + pinCount;
-		$('#'+selectedFrameID).attr('points', newPoints);
-		$('#'+selectedFrameID).text(newPoints.toString());
-		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		ball = parseInt(ball) + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-		ball = ball + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-	});
-	$('#pin5').on('click', function() {
-		$(this).toggleClass('pinDown')
-		var currentSelectedItem = selectedFrameID.split("-");
-		var game = currentSelectedItem[0];
-		var player = currentSelectedItem[1];
-		var frame = currentSelectedItem[2];
-		var ball = currentSelectedItem[3];
-		
-		var currentPinsDown = $('#'+selectedFrameID).attr('pins').split("-");
-		var pin1 = currentPinsDown[0];
-		var pin2 = currentPinsDown[1];
-		var pin3 = currentPinsDown[2];
-		var pin4 = currentPinsDown[3];
-		var pin5 = currentPinsDown[4];
-		if (pin5 == "0") {
-			//Add 2 pins as pin was up
-			pin5 = "1";
-			pinCount = 2;
-		} 
-		else {
-			//Subtract 2 pins as pin was down
-			pin5 = "0";
-			pinCount = -2;
-		}
-		
-		var currentPoints = $('#'+selectedFrameID).attr('points');
-		var newPoints = parseInt(currentPoints) + pinCount;
-		$('#'+selectedFrameID).attr('points', newPoints);
-		$('#'+selectedFrameID).text(newPoints.toString());
-		$('#'+selectedFrameID).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		ball = parseInt(ball) + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-		ball = ball + 1;
-		if (ball < 4 ) {
-			$('#'+game+'-'+player+'-'+frame+'-'+ball.toString()).attr('pins',pin1+"-"+pin2+"-"+pin3+"-"+pin4+"-"+pin5);
-		}
-	});
+	}
 });
 
-function onChangeCalculateAllFrameTotals() {
 
-}
+
